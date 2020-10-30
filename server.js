a2aa12dea0928f0c3103e2c1179dcb8190d0aafd
qiuwenwu <573242395@qq.com>
2020-10-30 18:34:57 +0800

    更新

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('mm_expand');
 // 修改程序运行目录
 $.runPath = __dirname  + $.slash;
-const Soa = require('../index.js');
+const Soa = require('mm_soa');
 
 var NODE_ENV = process.env.NODE_ENV || 'local';
 var config = `./config/${NODE_ENV}.json`.loadJson();
